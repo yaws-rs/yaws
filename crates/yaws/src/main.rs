84407f2f4fe5a90a3864abc0251980c7bf519d5c
pinkforest <36498018+pinkforest@users.noreply.github.com>
2023-01-15 13:04:25 +1100

    yak

--- crates/yaws/src/main.rs
+++ crates/yaws/src/main.rs
@@ -1,11 +1,13 @@
-mod http;
+//mod http;
 
 fn main() {
-    
+
+    /*
     tokio_uring::start(async move {
 
         let http_listener = http::Listener::new();
         http_listener.next().await;
         
     });
+     */
 }
